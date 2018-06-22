11ecf23b65137568df557481db3a9867f62889b1
unknown <ian@esportscreator.com>
2018-06-22 14:01:23 +0200

    organised the initial bot stuff better

--- src/server.js
+++ src/server.js
@@ -3,36 +3,43 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 let EchoCommand = require('./commands/echo.js');
-const config = {
-    commandDir: __dirname + '\\commands\\'
-}
 
+/**
+ * Basic config
+ * @todo move into it's own file
+ *
+ * @type {{commandDir: string, prefix: string}}
+ */
+const config = {
+    commandDir: __dirname + '\\commands\\',
+    prefix: '/'
+};
+
+// Login with the bot
 client.login(process.env.DISCORD_TOKEN);
 
-client.on('ready', function(event) {
+// Once ready give some feedback
+client.on('ready', function (event) {
     console.log('Harmony listening in.')
 });
 
-client.on('message', function(message) {
+// Filter for commands and pass them through
+client.on('message', function (message) {
 
-    let content = message.content;
+    // If it's not a command or the author is a bot don't do anything
+    if (!message.content.startsWith(config.prefix) || message.author.bot === true)
+        return;
 
-    // If the message comes from the user
-    if(content.startsWith('/') && message.author.bot === false)
-    {
-        // replace and split command
-        let command = content.replace('/', '').split(' ')[0],
-            commandPath = `${config.commandDir}${command}.js`;
+    let content = message.content,
+        command = content.replace('/', '').split(' '),
+        commandPath = `${config.commandDir}${command[0]}.js`;
 
-        // If the command actually exists
-        if(fs.existsSync(commandPath))
-        {
-            let c = new (require(commandPath));
-            c.run(message);
-        }
-        else
-        {
-            console.log('No command found', __dirname + `\\commands\\${command}.js`);
-        }
+    // If the command actually exists
+    if (fs.existsSync(commandPath)) {
+        let c = new (require(commandPath));
+        c.run(message);
+    }
+    else {
+        console.log('No command found', __dirname + `\\commands\\${command}.js`);
     }
 });

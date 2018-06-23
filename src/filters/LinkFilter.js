ea03fd99ca558b36a613a8262a81e6f25041f6b8
Ian <hi@ian.pe>
2018-06-23 18:23:43 +0200

    Link filtering for admins

--- src/filters/LinkFilter.js
+++ src/filters/LinkFilter.js
@@ -11,6 +11,12 @@ class LinkFilter {
         if(this.m.author.bot)
             return;
 
+        let mutedRole = this.m.member.roles.find(role => role.name === "admin");
+
+        if(mutedRole)
+            return;
+
+
         if (this.message.includes('https://') || this.message.includes('http://') || this.message.includes('www.')) {
             this.m.delete();
             console.log('Link found deleting');
